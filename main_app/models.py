3c95fddce4db10070e2b03e90310c6533cc8e7a2
Nate <natemausert@gmail.com>
2021-11-23 13:04:42 -0500

    added to models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+COMPLETE = (
+  ('Y', 'Yes'),
+  ('N', 'No')
+)
+
+class Resource(models.Model):
+  name = models.CharField(max_length=50)
+  url = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
+  def get_absolute_url(self):
+      return reverse("resources_detail", kwargs={"pk": self.id})
+
+
+class Project(models.Model):
+  name = models.CharField(max_length=20)
+  programming_language = models.Charfield(max_length=20)
+  description = models.TextField(max_length=250)
+  resources = models.ManyToManyField(Resource)
+  # steps = models.ManyToOneRel()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  complete = models.BooleanField
+
+class Step(models.Model):
+  name = models.CharField(max_length=200)
+  content = models.TextField(blank=True)
+  date = models.DateField('Creation date')
+  step = models.CharField()
+
+  class Meta:
+    ordering = ['-date']
+
+  def __str__(self):
+    return f'{self.get_step_display()} on {self.date}'
+
+  # def updated_today(self):
+  #   return self.step_set.filter(date=date.today()).count() >= len()
+  # Find a way to show if project was updated today based off if a step was added today
+
+class Wireframe(models.Model):
+  url = models.CharField(max_length=250)
+  project = models.OneToOneField(Project, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'Wireframe for the project_id: {self.project_id} @{self.url}'
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Project
+import uuid
+import boto3
 
 # Create your views here.
 
 from django.http import HttpResponse
 
-def home(request):
-  return HttpResponse('')
+class Home(request):
+  template_name = 'home.html'
+
+def about(request):
+  return render(request, 'about.html')
+
+def projects_index(request):
+  projects = Project.objects.filter(user=request.user)
+  return render(request, 'projects/index.html', {'projects': projects})
+
+def projects_detail(request, project_id):
+  project = Project.objects.get(id=project_id)
+  
