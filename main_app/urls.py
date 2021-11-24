ba0aa5b04a9415e25c7af902520ba587dfbd764e
Nate <natemausert@gmail.com>
2021-11-23 21:26:43 -0500

    filled out urls.py and views.py in main_app

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,36 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  # localhost:8000/
   path('', views.Home.as_view(), name='home'),
+  # localhost:8000/about/
+  path('about/', views.about, name='about'),
+  # localhost:8000/projects/
+  path('projects/', views.projects_index, name='projects_index'),
+  # localhost:8000/projects/:project_id
+  path('projects/<int:project_id>/', views.projects_detail, name='projects_detail'),
+  # localhost:8000/projects/create
+  path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
+  # localhost:8000/projects/
+  path('projects/<int:pk>/update', views.ProjectUpdate.as_view(), name="projects_update"),
+  # localhost:8000/projects/:pk/delete
+  path('projects/<int:pk>/delete', views.ProjectDelete.as_view(), name="projects_delete"),
+  # localhost:8000/projects/:project_id/add_step/
+  path('projects/<int:project_id>/add_step/', views.add_step, name="add_step"),
+  # localhost:8000/resources/create/
+  path('resources/create/', views.ResourceCreate.as_view(), name='resources_create'),
+  # localhost:8000/resources/:pk/
+  path('resources/<int:pk>/', views.ResourceDetail.as_view(), name='resources_detail'),
+  # localhost:8000/resources/
+  path('resources/', views.ResourceList.as_view(), name='resources_index'),
+  # localhost:8000/resources/:pk/update/
+  path('resources/<int:pk>/update/', views.ResourceUpdate.as_view(), name='resources_update'),
+  # localhost:8000/resources/:pk/delete/
+  path('resources/<int:pk>/delete/', views.ResourceDelete.as_view(), name='resources_delete'),
+  # localhost:8000/projects/:project_id/assoc_resource/:resource_id/
+  path('projects/<int:project_id>/assoc_resource/<int:resource_id>/', views.assoc_resource, name='assoc_resource'),
+  # localhost:8000/projects/project_id/add_wireframe/
+  path('projects/<int:project_id>/add_wireframe/', views.add_wireframe, name="add_wireframe"),
+  # localhost:8000/accounts/signup/
+  path("accounts/signup/", views.signup, name="signup")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.db.models import fields
 from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -7,24 +8,108 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Project
+from .models import Project, Step, Resource, Wireframe
+from .forms import StepForm
 import uuid
 import boto3
 
+# add S3_BASE_URL here
+# add BUCKET here
+
 # Create your views here.
-
-from django.http import HttpResponse
-
 class Home(LoginView):
   template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def projects_index(request):
   projects = Project.objects.filter(user=request.user)
   return render(request, 'projects/index.html', {'projects': projects})
 
+@login_required
 def projects_detail(request, project_id):
   project = Project.objects.get(id=project_id)
-  
+  resources_not_in_project = Resource.objects.exclude(id__in = project.resources.all().values_list('id'))
+  step_form = StepForm()
+  return render(request, 'projects/detail.html', {'project': project, 'step_form': step_form, 'resources': resources_not_in_project})
+
+class ProjectCreate(LoginRequiredMixin, CreateView):
+  model = Project
+  fields = ['name', 'programming_language', 'description']
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class ProjectUpdate(LoginRequiredMixin, UpdateView):
+  model = Project
+  fields = ['name', 'programming_language', 'description']
+
+class ProjectDelete(LoginRequiredMixin, DeleteView):
+  model = Project
+  success_url = '/projects/'
+
+@login_required
+def add_step(request, project_id):
+  form = StepForm(request.POST)
+  if form.is_valid():
+    new_step = form.save(commit=False)
+    new_step.project_id = project_id
+    new_step.save()
+  return redirect('projects_detail', project_id=project_id)
+
+class ResourceCreate(LoginRequiredMixin, CreateView):
+  model = Resource
+  fields = '__all__'
+
+class ResourceList(LoginRequiredMixin, ListView):
+  model = Resource
+
+class ResourceDetail(LoginRequiredMixin, DetailView):
+  model = Resource
+
+class ResourceUpdate(LoginRequiredMixin, UpdateView):
+  model = Resource
+  fields = ['name', 'url']
+
+class ResourceDelete(LoginRequiredMixin, DeleteView):
+  model = Resource
+  success_url = '/resources/'
+
+@login_required
+def assoc_resource(request, project_id, resource_id):
+  Project.objects.get(id=project_id).resources.add(resource_id)
+  return redirect('projects_detail', project_id=project_id)
+
+@login_required
+def add_wireframe(request, project_id):
+  wireframe_file = request.FILES.get('wireframe-photo', None)
+  if wireframe_file:
+    s3 = boto3.client('s3')
+    key = uuid.uuid4().hex + wireframe_file.name[wireframe_file.name.rfind('.'):]
+    try:
+      s3.upload_fileobj(wireframe_file, BUCKET, key)
+      wireframe = Wireframe(url=url, project_id=project_id)
+      project_wireframe = Wireframe.objects.filter(project_id = project_id)
+      if project_wireframe.first():
+        project_wireframe.first().delete()
+      wireframe.save()
+    except Exception as err:
+      print('An error occured sending a picture to s3 dipshit: %s' %err)
+  return redirect('projects_detail', project_id=project_id)
+
+def signup(request):
+  error_message = ""
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('projects_index')
+    else:
+      error_message='I have no idea how you messed up typing words into a box, but you did. Try again einstein. If you get it right this time your reward is a new account and no further embarrassment.'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
