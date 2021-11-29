2d385365b28ccae59af18119942eb4c42bf914dd
Nate <natemausert@gmail.com>
2021-11-29 12:32:58 -0500

    deploy 2 electric boogaloo

--- codingcooler/settings.py
+++ codingcooler/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('fuckyou&theconsolelogerrormessageyourodeinon41396719ru1094ug9qghj4vp;BFVI832YR[1p0;fh3gfh238O9GY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True'
