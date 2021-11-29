a215ea9e321ca5735b791cceb738f19b9c3a25d1
Nate <natemausert@gmail.com>
2021-11-28 21:04:05 -0500

    fixed functionaility. Had to drop db. I still feel only pain and rage however.

--- codingcooler/settings.py
+++ codingcooler/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'codingcooler.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'codingcooler',
+        'NAME': 'codingcooler2',
     }
 }
 
